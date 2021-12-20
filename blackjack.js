6cc033a326684569b0b4822023198854958771c3
ADMIN <nickiemaundu19@gmail.com>
2021-12-20 12:27:16 +0300

    updated the start game function and added a newcard function

--- blackjack.js
+++ blackjack.js
@@ -1,27 +1,40 @@
 let firstCard = 10
-let secondCard = 10
+let secondCard = 11
 let cardSum = firstCard + secondCard
 let hasBlackJack = false
 let message = " "
 let newMessage = document.getElementById("message-el")
-
+let sumEl = document.getElementById("resultSum")
+let cardEl = document.getElementById("carlds-el")
 function playGame() {
     // create logic of the blackjack game
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+    cardEl.innerText = "Cards: " + firstCard + " ; " + secondCard
+    sumEl.innerText = "Sum: " + cardSum
+    if (cardSum <= 20) {
+        message = "Do you want to draw a new card? 🤪🤪"
 
 
-    } else if (sum === 21) {
+
+    } else if (cardSum === 21) {
         // === strictly equals to , = assignement
-        message = " Woho, you got BlackJack! "
+        message = " Woho, you got BlackJack! 😃😃"
         hasBlackJack = true
 
     } else {
-        message = "You are out of the game! "
+        message = "You are out of the game! 😒😒"
+
 
     }
     // logout the message
     // console.log(message)
     newMessage.textContent = message
 
+}
+
+//newcard function
+function newCard() {
+    let card = 7
+    cardSum += card
+
+    playGame()
 }
